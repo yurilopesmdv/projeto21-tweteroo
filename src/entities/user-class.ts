1d8aa719344d820519820b8696e5797a1ffa1deb
Yuri Lopes <yurilopesm@hotmail.com>
2023-08-17 13:43:25 -0300

    feat: entities created

---
+++ src/entities/user-class.ts
@@ -0,0 +1,22 @@
+export class User {
+  private _username: string;
+  private _avatar: string;
+
+  constructor(username: string, avatar: string) {
+    this._username = username;
+    this._avatar = avatar;
+  }
+
+  get username(): string {
+    return this._username;
+  }
+  set username(username: string) {
+    this._username = username;
+  }
+  get avatar() {
+    return this._avatar;
+  }
+  set avatar(avatar: string) {
+    this._avatar = avatar;
+  }
+}
